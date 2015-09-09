43a6ca719997e9d27851ebe50371e14d2efeff6a
Val <valpreacher@gmail.com>
2015-09-10 02:56:31 +0300

    additional tests

--- test/index.test.js
+++ test/index.test.js
@@ -25,7 +25,7 @@ describe('Webpack replace loader ...', function () {
               loader: '__this',
               query: {
                 subject: 'var value',
-                replacement: 'var v'
+                replacement: 'var a'
               }
             }
           ]
@@ -38,7 +38,7 @@ describe('Webpack replace loader ...', function () {
           expect(error).to.equal(null);
           expect(contents).to.be.a('string');
           expect(contents.indexOf('var value')).to.equal(-1);
-          expect(contents.indexOf('var v')).to.not.equal(-1);
+          expect(contents.indexOf('var a')).to.not.equal(-1);
           done();
         });
       }
@@ -60,7 +60,7 @@ describe('Webpack replace loader ...', function () {
               loader: '__this',
               query: {
                 subject: 'var VALUE = \'\.*\'',
-                replacement: 'var v = \'\'',
+                replacement: 'var a = \'\'',
                 flags: 'i'
               }
             }
@@ -74,7 +74,7 @@ describe('Webpack replace loader ...', function () {
           expect(error).to.equal(null);
           expect(contents).to.be.a('string');
           expect(contents.indexOf('var value')).to.equal(-1);
-          expect(contents.indexOf('var v = \'\'')).to.not.equal(-1);
+          expect(contents.indexOf('var a = \'\'')).to.not.equal(-1);
           done();
         });
       }
@@ -96,7 +96,7 @@ describe('Webpack replace loader ...', function () {
               loader: '__this',
               query: {
                 subject: 'var value',
-                replacement: 'var v'
+                replacement: 'var a'
               }
             },
             {
@@ -117,8 +117,44 @@ describe('Webpack replace loader ...', function () {
           expect(error).to.equal(null);
           expect(contents).to.be.a('string');
           expect(contents.indexOf('var value')).to.equal(-1);
-          expect(contents.indexOf('var v')).to.not.equal(-1);
+          expect(contents.indexOf('var a')).to.not.equal(-1);
           expect(contents.indexOf('var bar')).to.not.equal(-1);
+          done();
+        });
+      }
+    );
+  });
+
+  it('should replace using string query', function (done) {
+    webpack(
+      {
+        entry: entryFilePath,
+        output: {
+          path: outputDirPath,
+          filename: outputFileName
+        },
+        module: {
+          loaders: [
+            {
+              test: /\.js$/,
+              loaders: [
+                '__this?subject=var value&replacement=var a',
+                '__this?subject=module.exports = value&replacement=module.exports = a'
+              ]
+            }
+          ]
+        }
+      },
+      function (error, stats) {
+        expect(error).to.equal(null);
+
+        fs.readFile(outputFilePath, 'utf8', function (error, contents) {
+          expect(error).to.equal(null);
+          expect(contents).to.be.a('string');
+          expect(contents.indexOf('var value')).to.equal(-1);
+          expect(contents.indexOf('var a')).to.not.equal(-1);
+          expect(contents.indexOf('module.exports = value')).to.equal(-1);
+          expect(contents.indexOf('module.exports = a')).to.not.equal(-1);
           done();
         });
       }
