3e7e6668bd7bffcdc80bf71ca9e95a2822566878
sashless <saweniger@gmail.com>
2015-11-13 14:40:06 +0100

    can pass array as option to replace more at once
    can pass a complete array instead of just single values

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 var utils = require('loader-utils');
 
-module.exports = function (source) {
+module.exports = function(source) {
   this.cacheable();
+
   var query = utils.parseQuery(this.query);
+  for (var i = 0; i < query.replace.length; i++) {
+    var option = query.replace[i];
 
-  if (typeof query.search !== 'undefined' && typeof query.replace !== 'undefined') {
-    if (typeof query.flags !== 'undefined') {
-      query.search = new RegExp(query.search, query.flags);
-    }
-
-    source = source.replace(query.search, query.replace);
+    source = source.split(option.search).join(option.replace)
   }
 
   return source;
