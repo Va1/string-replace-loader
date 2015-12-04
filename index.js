b77bd43afdc62bd657c0c402815dd28b5112841f
sashless <saweniger@gmail.com>
2015-12-04 14:22:13 +0100

    support single replacement using regex + flags again

--- index.js
+++ index.js
@@ -4,10 +4,23 @@ module.exports = function(source) {
   this.cacheable();
 
   var query = utils.parseQuery(this.query);
-  for (var i = 0; i < query.replace.length; i++) {
-    var option = query.replace[i];
 
-    source = source.split(option.search).join(option.replace)
+  if (Array.isArray(query.replace)) {
+    for (var i = 0; i < query.replace.length; i++) {
+      var option = query.replace[i];
+
+      source = source.split(option.search).join(option.replace)
+    }
+
+    return source;
+  }
+
+  if (typeof query.search !== 'undefined' && typeof query.replace !== 'undefined') {
+    if (typeof query.flags !== 'undefined') {
+      query.search = new RegExp(query.search, query.flags);
+
+      return source.replace(query.search, query.replace);
+    }
   }
 
   return source;
